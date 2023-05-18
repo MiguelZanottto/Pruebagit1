14824f47613dd6eed99123e5317be72bccea4bda
MiguelZanottto <migzanotto18@hotmail.com>
2023-05-18 20:28:00 +0200

    Version 2 - de pena

--- PruebaGitHub/src/Test.java
+++ PruebaGitHub/src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Este es la version 1"); // Primera Version
+		System.out.println("Este es la version 2"); // Segunda Version
 	}
 
 }
